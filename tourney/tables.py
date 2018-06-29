c6d75ed9ce0500fccb596fd276af9d396fcf6299
Joe Dietz <captdietz@gmail.com>
2018-06-28 23:15:14 -0400

    table formating for scorecards

--- tourney/tables.py
+++ tourney/tables.py
@@ -87,20 +87,30 @@ class MessageTable(tables.Table):
 
 		
 class ScoreCardTable(tables.Table):
-	net_day_points = tables.Column(
-		verbose_name = 'Daily Net', 
-		attrs={
-		"th": {"id": "foo"},
-		"td": {"align": "center"}
-		}	
-		)
-	thru = tables.Column(	
-		verbose_name = 'Thru', 
-		attrs={
-		"th": {"id": "foo"},
-		"td": {"align": "center"}
-		}	
-		)
+	net_day_points = tables.Column(verbose_name = 'Daily Net',)
+	thru = tables.Column(verbose_name = 'Thru',)
+	h1_pts = tables.Column(verbose_name = 'H1',)
+	h2_pts = tables.Column(verbose_name = 'H2',)
+	h3_pts = tables.Column(verbose_name = 'H3',)
+	h4_pts = tables.Column(verbose_name = 'H4',)
+	h5_pts = tables.Column(verbose_name = 'H5',)
+	h6_pts = tables.Column(verbose_name = 'H6',)
+	h7_pts = tables.Column(verbose_name = 'H7',)
+	h8_pts = tables.Column(verbose_name = 'H8',)
+	h9_pts = tables.Column(verbose_name = 'H9',)
+	h10_pts = tables.Column(verbose_name = 'H10',)
+	h11_pts = tables.Column(verbose_name = 'H11',)
+	h12_pts = tables.Column(verbose_name = 'H12',)
+	h13_pts = tables.Column(verbose_name = 'H13',)
+	h14_pts = tables.Column(verbose_name = 'H14',)
+	h15_pts = tables.Column(verbose_name = 'H15',)
+	h16_pts = tables.Column(verbose_name = 'H16',)
+	h17_pts = tables.Column(verbose_name = 'H17',)
+	h18_pts = tables.Column(verbose_name = 'H18',)
+	raw_day_points = tables.Column(verbose_name = 'Daily Raw',)
+	r1_score = tables.Column(verbose_name = 'Round 1',)
+	r2_score = tables.Column(verbose_name = 'Round 2',)
+	net_tourney_score = tables.Column(verbose_name = 'Tourney',)
 		
 	class Meta:
 		model = Daily
