89d570210be7355becf6e22f145abc2aa0acbe90
Joe Dietz <captdietz@gmail.com>
2022-05-14 22:37:14 -0400

    remove openGoogle for Oauth

--- buddies/settings.py
+++ buddies/settings.py
@@ -84,7 +84,7 @@ TEMPLATES = [
 
 AUTHENTICATION_BACKENDS = (
 	'social_core.backends.open_id.OpenIdAuth',  # for Google authentication
-	'social_core.backends.google.GoogleOpenId',  # for Google authentication
+	#'social_core.backends.google.GoogleOpenId',  # for Google authentication
 	'social_core.backends.google.GoogleOAuth',
 	'social_core.backends.google.GoogleOAuth2',  # for Google authentication
 	'django.contrib.auth.backends.ModelBackend',
