e78f7c9e4f9d8e7713271744f600bb3e05b6058a
Joe Dietz <captdietz@gmail.com>
2022-09-10 11:52:32 -0400

    Goleta Lights

--- tourney/forms.py
+++ tourney/forms.py
@@ -121,8 +121,8 @@ class ReverseCompileForm(forms.Form):
 	
 class LightsForm(forms.ModelForm):
 	creator = forms.CharField(label='Your Name', max_length=15)
-	zoneRoof = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, choices=COLOR_CHOICES)
-	zonePalms = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, choices=COLOR_CHOICES)
+	zoneRoof = forms.MultipleChoiceField(widget=forms.SelectMultiple, choices=COLOR_CHOICES)
+	zonePalms = forms.MultipleChoiceField(widget=forms.SelectMultiple, choices=COLOR_CHOICES)
 	
 	class Meta:
 		model = Lights
