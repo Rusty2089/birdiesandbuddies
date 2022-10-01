67763f9fed80dcbc4eee154632721a70a2a1f55e
Joe Dietz <captdietz@gmail.com>
2022-10-01 16:19:16 -0400

    Goleta Lights

--- tourney/views/views_main.py
+++ tourney/views/views_main.py
@@ -713,7 +713,7 @@ def goletalights_json(request):
 	RoofEffect = qs.RoofEffect
 	PalmColors = qs.PalmColors
 	PalmEffect = qs.PalmEffect
-	return render(request, 'tourney/goletalights_json.html', {'creator': creator, 'RoofColors': RoofColors, 'RoofEffect': RoofEffect, 'PalmColors': PalmColors, 'PalmEffect': PalmEffect})
+	return render(request, 'tourney/goletalights_json.html', {'creator': creator, 'RoofColors': RoofColors, 'RoofEffect': RoofEffect, 'PalmColors': PalmColors, 'PalmEffect': PalmEffect, 'FloodColors': FloodColors, 'SconceColors': SconceColors})
 	
 def goletalights(request):
 	form = LightsForm(request.POST or None)
