f31e9e84c5ef826396d987f2f69e3bfc34250b96
Joe Dietz <captdietz@gmail.com>
2023-06-03 09:18:41 -0400

    adding room teams

--- tourney/admin.py
+++ tourney/admin.py
@@ -8,10 +8,16 @@ from .models import Extra
 from .models import Light
 
 class ProfileAdmin(admin.ModelAdmin):
-	list_display = [field.name for field in Profile._meta.get_fields()  if not field.is_relation]
+	list_display = ('display_name', 'first_name', 'last_name', 'isgolfing', 'r1_quota', 'r1_group', 'r1_team', 'team', 'room_team')
+	list_editable = ('display_name', 'first_name', 'last_name', 'isgolfing', 'r1_quota', 'r1_group', 'r1_team', 'team', 'room_team')
+	
+class DailyAdmin(admin.ModelAdmin):
+	list_display = [field.name for field in Daily._meta.get_fields()  if not field.is_relation]
+	list_editable = [field.name for field in Daily._meta.get_fields()  if not field.is_relation]
+
 admin.site.register(Profile, ProfileAdmin)
 admin.site.register(Course)
-admin.site.register(Daily)
+admin.site.register(Daily, DailyAdmin)
 admin.site.register(Message)
 admin.site.register(Extra)
 admin.site.register(Light)
